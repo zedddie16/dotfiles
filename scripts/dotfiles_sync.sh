fe218c5f5b465dd9b80c7af2f68eb4e42bfbebba
zedddie16 <zedddiezxc@gmail.com>
2025-06-19 01:08:52 +0200

    Dotfiles Sync: 2025-06-19_01:08:51

--- scripts/dotfiles_sync.sh
+++ scripts/dotfiles_sync.sh
@@ -15,6 +15,11 @@ cp -r waybar $DOTFILES_DIR
 cp cava/config "$DOTFILES_DIR/cava/"
 cp ~/.zshrc $DOTFILES_DIR
 
+cp /home/tuturuu/.config/Cursor/User/keybindings.json $DOTFILES_DIR/cursor/
+cp /home/tuturuu/.config/Cursor/User/settings.json $DOTFILES_DIR/cursor/
+
+
+
 cd $DOTFILES_DIR
 git add .
 git commit -m "Dotfiles Sync: $NOW"
