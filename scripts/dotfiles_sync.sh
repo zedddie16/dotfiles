6c6d53c9c6d0ed3ec136204832a57911039c5439
zedddie16 <zedddiezxc@gmail.com>
2025-12-05 09:51:42 +0100

    Dotfiles Sync: 2025-12-05_09:51:41

--- scripts/dotfiles_sync.sh
+++ scripts/dotfiles_sync.sh
@@ -5,6 +5,7 @@ cd ~/.config || exit 1
 NOW=$( date '+%F_%H:%M:%S' )
 DOTFILES_DIR="$HOME/archbackup/dotfiles/"
 
+cp -r fastfetch $DOTFILES_DIR
 cp -r ghostty $DOTFILES_DIR
 cp -r kitty $DOTFILES_DIR
 cp -r rofi $DOTFILES_DIR
@@ -12,7 +13,6 @@ cp -r tmux $DOTFILES_DIR
 cp -r hypr $DOTFILES_DIR
 cp -r ~/dev/scripts/ $DOTFILES_DIR
 cp -r waybar $DOTFILES_DIR
-cp cava/config "$DOTFILES_DIR/cava/"
 cp ~/.zshrc $DOTFILES_DIR
 
 cp /home/tuturuu/.config/Cursor/User/keybindings.json $DOTFILES_DIR/cursor/
